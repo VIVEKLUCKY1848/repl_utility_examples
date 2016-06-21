e65fe8dfa72df6f4e62be9d662c7b16783d81fb3
VS <VIVEKLUCKY1848@users.noreply.github.com>
2016-06-21 22:38:29 +0530

    Update repl_examples.bat

--- repl_examples.bat
+++ repl_examples.bat
@@ -9,3 +9,7 @@ for /r %%F in (*.sql) do (
   type "%%F"|repl "C:\" "E:\" LI >"%%F.new"
   move /y "%%F.new" "%%F" >nul
 )
+
+for /r %F in (*.sql) do @(type "%F"|repl "C:\" "E:\" >"%F.new"&move /y "%F.new" "%F" L >nul)
+:: Case Insensitive use I switch
+for /r %F in (*.sql) do @(type "%F"|repl "C:\" "E:\" I >"%F.new"&move /y "%F.new" "%F" L >nul)
